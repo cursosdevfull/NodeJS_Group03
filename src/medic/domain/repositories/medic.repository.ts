55656cef293a7251d711de60fe57ca697ceabd95
Sergio <sergiohidalgocaceres@gmail.com>
2020-11-01 06:34:34 -0500

    update diagram class 01

--- src/medic/domain/repositories/medic.repository.ts
+++
@@ -1,9 +0,0 @@
-import { Medic } from '../entities/medic.entity';
-
-export interface MedicRepository {
-  insert(medic: Medic): Medic;
-  update(id: string | number, medic: Medic): Medic;
-  get(isActive: boolean): Array<Medic>;
-  getOne(id: string | number): Medic;
-  delete(id: string | number): Medic;
-}
